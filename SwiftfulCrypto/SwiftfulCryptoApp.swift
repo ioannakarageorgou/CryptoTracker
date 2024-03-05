a5ef3092c0ba81c1c65e164eca979738a1346bf2
Ioanna Karageorgou <ioannakarageorgou@gmail.com>
2024-03-05 13:27:06 +0200

    Set up project. add color theme

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Ioanna Karageorgou on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
