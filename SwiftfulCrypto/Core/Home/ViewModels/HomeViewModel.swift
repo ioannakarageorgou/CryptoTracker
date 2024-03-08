c386ab4892e5fc146afa77ab9a8c7fe89aa0fb92
Ioanna Karageorgou <ioannakarageorgou@gmail.com>
2024-03-08 17:42:14 +0200

    Add dynamic list to display coins

---
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewModel.swift
+//  SwiftfulCrypto
+//
+//  Created by Ioanna Karageorgou on 8/3/24.
+//
+
+import Foundation
+
+class  HomeViewModel: ObservableObject {
+
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+}
