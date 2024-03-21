b7cf9e4820c1b2688524a8a9bad620755afe4fe9
Ioanna Karageorgou <ioannakarageorgou@gmail.com>
2024-03-21 16:38:43 +0200

    Add reusable Networking layer

---
+++ SwiftfulCrypto/Utilities/NetworkingManager.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkingManager.swift
+//  SwiftfulCrypto
+//
+//  Created by Ioanna Karageorgou on 21/3/24.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+
+    static func download(url: URL) -> AnyPublisher<Data, any Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap({ try handleURLResponse(output: $0) })
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw URLError(.badServerResponse)
+        }
+        return output.data
+    }
+
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
