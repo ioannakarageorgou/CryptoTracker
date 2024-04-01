fc4a912cec30acd7bb4e27007bd2213411ceaad2
Ioanna Karageorgou <ioannakarageorgou@gmail.com>
2024-04-01 14:37:25 +0300

    Add settings screen

---
+++ SwiftfulCrypto/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,87 @@
+//
+//  SettingsView.swift
+//  SwiftfulCrypto
+//
+//  Created by Ioanna Karageorgou on 1/4/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+
+    let youtubeURL = URL(string: "https://www.youtube.com/c/swiftfulthinking")!
+    let coinGeckoURL = URL(string: "https://www.coingecko.com")!
+    let personalURL = URL(string: "https://github.com/ioannakarageorgou")!
+
+    var body: some View {
+        NavigationView {
+            List {
+                swiftfulThinkingSection
+                coinGeckoSection
+                developerSection
+            }
+            .font(.headline)
+            .accentColor(.blue)
+            .listStyle(.grouped)
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    XMarkButton()
+                }
+            }
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
+
+private extension SettingsView {
+    var swiftfulThinkingSection: some View {
+        Section(header: Text("Swiftful Thiinking")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was made by following a @SwiftfulThinking course on Youtube. It uses MVVM Architecture, Combine and Core Data")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Youtube Channel", destination: youtubeURL)
+        }
+    }
+
+    var coinGeckoSection: some View {
+        Section(header: Text("CoinGecko")) {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("The cryptocurrency data that is used in this app comes from a free API from CoinGecko!")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit CoinGecko", destination: coinGeckoURL)
+        }
+    }
+
+    var developerSection: some View {
+        Section(header: Text("Developer")) {
+                Text("This app was developed by Ioanna Karageorgou. It uses SwiftUI and is written 100% in Swift. The project benefits from multithreading, publishers/subscribers and data persistence.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            Link("Github Profile", destination: personalURL)
+        }
+    }
+}
